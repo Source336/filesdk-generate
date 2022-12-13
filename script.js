712b21024bdf8203c65727438ccf3d973142ed28
Source336 <115052285+Source336@users.noreply.github.com>
2022-12-13 13:10:48 -0300

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+var id = ""
+var sdk = "file_sdk_"
+var sdk2 = document.querySelector(".sdk .your-sdk")
+var copyBTN = document.querySelector("copy")
+
+var codeSupports = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+// There is a 12-digit ID SDK
+for (let i = 0; i < 12; i++) {
+    id += codeSupports[Math.floor(Math.random() * 36)]
+}
+
+sdk += id
+sdk2.innerHTML = sdk
+
+copyBTN.onclick = () => {
+    navigator.clipboard.writeText(sdk)
+}
